ce29533ebf3839210b0054dbb7f8d50613d9a38a
Jeremiah Matthews <jeremiah.matthews105@gmail.com>
2023-10-18 14:45:18 -0500

    first commit

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,20 @@
+import org.apache.commons.lang3.StringUtils; //does not come with java comes from a different library
+
+import java.util.Scanner;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in) ;
+
+        //System.out.println("Give me a string");
+        //String input = scanner.nextLine();
+        System.out.println("Enter Something: ");
+        String input = scanner.nextLine();
+        System.out.println("You entered: \"" + input + "\"");
+
+        System.out.println("Reverse Case: \"" + StringUtils.reverse(input) + "\"");
+        System.out.println(StringUtils.capitalize(input));
+
+    }
+
+}
